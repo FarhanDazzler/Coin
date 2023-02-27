2b39b72134d953cd81fe7df8599075afb506be1f
Eluri <Maheshwararao.Eluri-ext@ab-Inbev.com>
2023-02-27 12:41:48 +0530

    gcd lcd scope accordion

---
+++ src/redux/ControlData/ControlDataSaga.js
@@ -0,0 +1,40 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import {
+  ACTION_GET_CONTROL_DATA,
+  ACTION_GET_CONTROL_DATA_SUCCESS,
+  ACTION_GET_CONTROL_DATA_FAILED,
+} from '../types';
+import { Axios } from '../../api/axios';
+import { getSimplifiedError } from '../../utils/error';
+
+function getControlDataApiCall(data) {
+    console.log("saga", data.payload.data);
+    let params = data.payload.data
+  return Axios.get('/get_control_scope?ControlID=' + params.controlId + '&coOwner=' + params.coOwner);
+}
+
+function* getControlData(payload) {
+  try {
+    const response = yield getControlDataApiCall(payload);
+    console.log("response", response);
+    if (response?.success === true) {
+      yield put({
+        type: ACTION_GET_CONTROL_DATA_SUCCESS,
+        data: response?.data,
+      });
+    } else {
+      yield put({
+        type: ACTION_GET_CONTROL_DATA_FAILED,
+        message: "Somthing went wrong",
+      });
+    }
+  } catch (e) {
+    yield put({ type: ACTION_GET_CONTROL_DATA_FAILED, error: getSimplifiedError(e) });
+  }
+}
+
+function* controlData() {
+  yield takeEvery(ACTION_GET_CONTROL_DATA, getControlData);
+}
+
+export default controlData;
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import AssessmentSaga from './Assessments/AssessmentSaga';
 import AuthSaga from './Auth/AuthSaga';
+import controlData from './ControlData/ControlDataSaga';
 
 export function* sagas() {
-  yield all([AuthSaga, AssessmentSaga]);
+  yield all([AuthSaga, AssessmentSaga, controlData()]);
 }
