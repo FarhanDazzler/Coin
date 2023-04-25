f36c4935aeeef6a34efa468d10c76c58b1a595f0
Jaymin <jaymin.patel-ext@ab-inbev.com>
2023-04-25 22:32:06 +0530

    feat: add user role on the application

--- src/components/NewHomePage/index.jsx
+++ src/components/NewHomePage/index.jsx
@@ -12,7 +12,7 @@ const NewHomePage = () => {
   const history = useHistory();
   const query = new URLSearchParams(history.location.search);
   const Control_ID = query.get('Control_ID');
-
+  const userRole = localStorage.getItem('user_Role');
   const { accounts } = useMsal();
   return (
     <div>
@@ -21,9 +21,10 @@ const NewHomePage = () => {
           <div className="row pt-5 align-items-center">
             <div className="col-lg-4">
               <h4 className="welcome-text">Welcome</h4>
-              <h2 className="user-name-home yellow-gradient-text">
+              <h2 className="user-name-home yellow-gradient-text mb-2">
                 {accounts.length > 0 ? accounts[0].name.split('(').join(' (') : 'User Name'}
               </h2>
+              {userRole && <h3 className="user-role">{userRole}</h3>}
             </div>
             <div className="col-lg-8">
               <div className="mb-4">
