4e462705a3b41cb296fd3988463a480abe7cd5f7
Kuldeep Singh <Kuldeep.singh@ab-inbev.com>
2023-04-25 12:20:57 +0530

    MDM MICS Framework Fix and Assessment Bank Landing page

---
+++ src/pages/AssessmentBank/ScheduleSurvey/ScheduleSurveyPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import PageWrapper from '../../../components/wrappers/PageWrapper';
+import '../AssessmentBankLandingPage.scss';
+const ScheduleSurveyPage = () => {
+  return (
+    <PageWrapper>
+      <div className="container py-5">
+        <div className="col-lg-12 py-4 AssessmentBankBoxWrapper"></div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default ScheduleSurveyPage;
