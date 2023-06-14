15f711d000a75a70094fb05b7815d5d4b8be1e81
Singh <kuldeep.singh@ab-inbev.com>
2023-06-14 17:27:50 +0530

    Add Function Table RL MDM

--- src/redux/REP_Letters/RLMDM/RLMDMReducer.js
+++ src/redux/REP_Letters/RLMDM/RLMDMReducer.js
@@ -46,11 +46,36 @@ export const ACTION_UPDATE_ORGANIZATIONAL_MD_DATA_FAILED =
   'ACTION_UPDATE_ORGANIZATIONAL_MD_DATA_FAILED';
 
 // =================== UPDATE Organisational MD ========================//
+
 // =================== Assign Repp Letter Functional Master DATA ========================//
 export const ASSIGN_RL_FUNCTIONAL_MASTERDATA_REQUEST = 'ASSIGN_RL_FUNCTIONAL_MASTERDATA_REQUEST';
 export const ASSIGN_RL_FUNCTIONAL_MASTERDATA_SUCCESS = 'ASSIGN_RL_FUNCTIONAL_MASTERDATA_SUCCESS';
 export const ASSIGN_RL_FUNCTIONAL_MASTERDATA_ERROR = 'ASSIGN_RL_FUNCTIONAL_MASTERDATA_ERROR';
 // =================== Assign Repp Letter Functional Master DATA ========================//
+
+// =================== GET RL ZONE DATA ========================//
+export const GET_RL_ZONE_REQUEST = 'GET_RL_ZONE_REQUEST';
+export const GET_RL_ZONE_SUCCESS = 'GET_RL_ZONE_SUCCESS';
+export const GET_RL_ZONE_ERROR = 'GET_RL_ZONE_ERROR';
+// =================== GET RL ZONE DATA ========================//
+
+// =================== GET RL BU FROM ZONE DATA ========================//
+export const GET_RL_BU_FROM_ZONE_REQUEST = 'GET_RL_BU_FROM_ZONE_REQUEST';
+export const GET_RL_BU_FROM_ZONE_SUCCESS = 'GET_RL_BU_FROM_ZONE_SUCCESS';
+export const GET_RL_BU_FROM_ZONE_ERROR = 'GET_RL_BU_FROM_ZONE_ERROR';
+// =================== GET RL BU FROM ZONE DATA ========================//
+
+// =================== GET RL FUNCTIONS REQUEST ========================//
+export const GET_RL_FUNCTIONS_REQUEST = 'GET_RL_FUNCTIONS_REQUEST';
+export const GET_RL_FUNCTIONS_SUCCESS = 'GET_RL_FUNCTIONS_SUCCESS';
+export const GET_RL_FUNCTIONS_ERROR = 'GET_RL_FUNCTIONS_ERROR';
+// =================== GET RL FUNCTIONS REQUEST ========================//
+
+// =================== ADD Repp Letter Functional Master DATA ========================//
+export const ADD_RL_FUNCTIONAL_MASTERDATA_REQUEST = 'ADD_RL_FUNCTIONAL_MASTERDATA_REQUEST';
+export const ADD_RL_FUNCTIONAL_MASTERDATA_SUCCESS = 'ADD_RL_FUNCTIONAL_MASTERDATA_SUCCESS';
+export const ADD_RL_FUNCTIONAL_MASTERDATA_ERROR = 'ADD_RL_FUNCTIONAL_MASTERDATA_ERROR';
+// =================== ADD Repp Letter Functional Master DATA ========================//
 
 const block = {
   loading: false,
@@ -68,6 +93,10 @@ const initialState = {
   addOrganizationalMd: { ...block, data: [] },
   updateOrganizationalMd: { ...block, data: [] },
   rlAssignFunctionalMasterdata: { ...block, data: [] },
+  get_rep_zones: { ...block, data: [] },
+  get_rep_bu_form_zone: { ...block, data: [] },
+  get_rep_functions: { ...block, data: [] },
+  rlAddFunctionalMasterdata: { ...block, data: [] },
 };
 
 export const RLMDMReducer = (state = initialState, { type, payload = {} }) => {
@@ -175,7 +204,7 @@ export const RLMDMReducer = (state = initialState, { type, payload = {} }) => {
           loading: false,
         },
       };
-      case ACTION_GET_RL_PARENT_ENTITY_DATA_FAILED:
+    case ACTION_GET_RL_PARENT_ENTITY_DATA_FAILED:
       return {
         ...state,
         getRlParentEntityData: {
@@ -183,7 +212,7 @@ export const RLMDMReducer = (state = initialState, { type, payload = {} }) => {
           loading: false,
         },
       };
-      
+
     // MDM ASSIGN Functional MD data
     case ASSIGN_RL_FUNCTIONAL_MASTERDATA_REQUEST:
       return {
@@ -199,12 +228,11 @@ export const RLMDMReducer = (state = initialState, { type, payload = {} }) => {
           loading: false,
         },
       };
-      case ASSIGN_RL_FUNCTIONAL_MASTERDATA_ERROR:
+    case ASSIGN_RL_FUNCTIONAL_MASTERDATA_ERROR:
       return {
         ...state,
         rlAssignFunctionalMasterdata: { ...state.rlAssignFunctionalMasterdata, loading: false },
       };
-    
 
     // Add Organizational MD
     case ACTION_ADD_ORGANIZATIONAL_MD_DATA:
@@ -258,8 +286,79 @@ export const RLMDMReducer = (state = initialState, { type, payload = {} }) => {
           ...state.updateOrganizationalMd,
           loading: false,
         },
-    }
-    
+      };
+
+    // GEt RL Zone data
+    case GET_RL_ZONE_REQUEST:
+      return {
+        ...state,
+        get_rep_zones: { ...state.get_rep_zones, loading: true },
+      };
+    case GET_RL_ZONE_SUCCESS:
+      return {
+        ...state,
+        get_rep_zones: { ...state.get_rep_zones, data: payload, loading: false },
+      };
+    case GET_RL_ZONE_ERROR:
+      return {
+        ...state,
+        get_rep_zones: { ...state.get_rep_zones, loading: false },
+      };
+
+    // Get RL BU from Zone data
+    case GET_RL_BU_FROM_ZONE_REQUEST:
+      return {
+        ...state,
+        get_rep_bu_form_zone: { ...state.get_rep_bu_form_zone, loading: true },
+      };
+    case GET_RL_BU_FROM_ZONE_SUCCESS:
+      return {
+        ...state,
+        get_rep_bu_form_zone: { ...state.get_rep_bu_form_zone, data: payload, loading: false },
+      };
+    case GET_RL_BU_FROM_ZONE_ERROR:
+      return {
+        ...state,
+        get_rep_bu_form_zone: { ...state.get_rep_bu_form_zone, loading: false },
+      };
+
+    // Get RL Functions data
+    case GET_RL_FUNCTIONS_REQUEST:
+      return {
+        ...state,
+        get_rep_functions: { ...state.get_rep_functions, loading: true },
+      };
+    case GET_RL_FUNCTIONS_SUCCESS:
+      return {
+        ...state,
+        get_rep_functions: { ...state.get_rep_functions, data: payload, loading: false },
+      };
+    case GET_RL_FUNCTIONS_ERROR:
+      return {
+        ...state,
+        get_rep_functions: { ...state.get_rep_functions, loading: false },
+      };
+
+    // MDM ADD Functional MD data
+    case ADD_RL_FUNCTIONAL_MASTERDATA_REQUEST:
+      return {
+        ...state,
+        rlAddFunctionalMasterdata: { ...state.rlAddFunctionalMasterdata, loading: true },
+      };
+    case ADD_RL_FUNCTIONAL_MASTERDATA_SUCCESS:
+      return {
+        ...state,
+        rlAddFunctionalMasterdata: {
+          ...state.rlAddFunctionalMasterdata,
+          data: payload,
+          loading: false,
+        },
+      };
+    case ADD_RL_FUNCTIONAL_MASTERDATA_ERROR:
+      return {
+        ...state,
+        rlAddFunctionalMasterdata: { ...state.rlAddFunctionalMasterdata, loading: false },
+      };
 
     default:
       return state;
