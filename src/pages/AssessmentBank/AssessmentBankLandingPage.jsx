0dbf93878262ac12e5a4ab31dcb79f3f67a6123f
Singh <kuldeep.singh@ab-inbev.com>
2023-07-28 13:36:31 +0530

    Added Loader Screen on Schedule assessment page

--- src/pages/AssessmentBank/AssessmentBankLandingPage.jsx
+++ src/pages/AssessmentBank/AssessmentBankLandingPage.jsx
@@ -8,36 +8,54 @@ import MoreTimeIcon from '@mui/icons-material/MoreTime';
 // Assessments Summary Table
 import AssessmentsSummaryTable from './Table/AssessmentsSummaryTable';
 
+import { addAssessmentSchedulingAndTriggeringSelector } from '../../redux/AssessmentBank/AssessmentBankSelectors';
+import { useSelector } from 'react-redux';
+import { DotSpinner } from '@uiball/loaders';
+
 const AssessmentBankLandingPage = () => {
   const history = useHistory();
+
+  const addAssessmentSchedulingAndTriggeringState = useSelector(
+    addAssessmentSchedulingAndTriggeringSelector,
+  );
   const handleSheduleSurvey = () => {
     history.push('/assessmentbank/schedule-survey');
   };
+
   return (
     <PageWrapper>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <div className="mt-5">
-              <div className="row">
-                <div className="col-12 col-lg-12">
-                  <Group spacing="xs" className="actions-button-wrapper">
-                    <Button
-                      color="silver"
-                      size="large"
-                      startIcon={<MoreTimeIcon />}
-                      onClick={handleSheduleSurvey}
-                    >
-                      Schedule Survey
-                    </Button>
-                  </Group>
+      {addAssessmentSchedulingAndTriggeringState.loading ? (
+        <div className="loader-animation">
+          <DotSpinner size={100} speed={0.9} color="#e3af32" />
+          <p className="loader-Desc ml-3">
+            Please wait while we are scheduling Assessments for you
+          </p>
+        </div>
+      ) : (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="mt-5">
+                <div className="row">
+                  <div className="col-12 col-lg-12">
+                    <Group spacing="xs" className="actions-button-wrapper">
+                      <Button
+                        color="silver"
+                        size="large"
+                        startIcon={<MoreTimeIcon />}
+                        onClick={handleSheduleSurvey}
+                      >
+                        Schedule Survey
+                      </Button>
+                    </Group>
+                  </div>
                 </div>
               </div>
+              <AssessmentsSummaryTable />
             </div>
-            <AssessmentsSummaryTable />
           </div>
         </div>
-      </div>
+      )}
     </PageWrapper>
   );
 };
