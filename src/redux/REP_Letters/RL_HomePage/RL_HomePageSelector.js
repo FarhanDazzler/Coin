ecb19ff406eab11d6d8c05fa02500b1b5dbc47ac
Singh <kuldeep.singh@ab-inbev.com>
2023-08-24 13:27:14 +0530

    Home page RL Function || Global

--- src/redux/REP_Letters/RL_HomePage/RL_HomePageSelector.js
+++ src/redux/REP_Letters/RL_HomePage/RL_HomePageSelector.js
@@ -1,4 +1,4 @@
 export const getFunctionRecipientHomePageDataSelector = (state) =>
   state?.RL_HomePage?.getFunctionRecipientHomePageData;
 export const getFunctionGlobalPersonaHomePageDataSelector = (state) =>
-  state?.RL_HomePage?.getFunctionRecipientHomePageData;
+  state?.RL_HomePage?.getFunctionGlobalPersonaHomePageData;
