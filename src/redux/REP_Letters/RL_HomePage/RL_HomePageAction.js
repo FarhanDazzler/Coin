ddea2e6faccab4e3023a6d4429f97a81d9aab835
Singh <kuldeep.singh@ab-inbev.com>
2023-09-01 17:07:05 +0530

    Redux step up functional rep letter form

--- src/redux/REP_Letters/RL_HomePage/RL_HomePageAction.js
+++ src/redux/REP_Letters/RL_HomePage/RL_HomePageAction.js
@@ -7,6 +7,10 @@ import {
   GET_BU_FINANCE_DIRECTOR_HOME_PAGE_TABLE_DATA_REQUEST,
   GET_BU_ZONE_CONTROL_HOME_PAGE_TABLE_DATA_REQUEST,
   GET_BU_ZONE_VP_HOME_PAGE_TABLE_DATA_REQUEST,
+  ADD_OR_UPDATE_FUNCTION_DRAFT_RESPONSE_REQUEST,
+  GET_LATEST_FUNCTION_DRAFT_RESPONSE_REQUEST,
+  GET_FUNCTION_SUBMIT_RESPONSE_REQUEST,
+  ADD_FUNCTION_SUBMIT_RESPONSE_REQUEST,
 } from './RL_HomePageReducer';
 
 export const getFunctionRecipientHomePageData = (payload) => ({
@@ -40,5 +44,25 @@ export const get_BU_Zone_ControlHomePageData = (payload) => ({
 });
 export const get_BU_Zone_VPHomePageData = (payload) => ({
   type: GET_BU_ZONE_VP_HOME_PAGE_TABLE_DATA_REQUEST,
+  payload,
+});
+
+export const addOrUpdateFunctionDraftResponse = (payload) => ({
+  type: ADD_OR_UPDATE_FUNCTION_DRAFT_RESPONSE_REQUEST,
+  payload,
+});
+
+export const getLatestFunctionDraftResponse = (payload) => ({
+  type: GET_LATEST_FUNCTION_DRAFT_RESPONSE_REQUEST,
+  payload,
+});
+
+export const getFunctionSubmitResponse = (payload) => ({
+  type: GET_FUNCTION_SUBMIT_RESPONSE_REQUEST,
+  payload,
+});
+
+export const addFunctionSubmitResponse = (payload) => ({
+  type: ADD_FUNCTION_SUBMIT_RESPONSE_REQUEST,
   payload,
 });
