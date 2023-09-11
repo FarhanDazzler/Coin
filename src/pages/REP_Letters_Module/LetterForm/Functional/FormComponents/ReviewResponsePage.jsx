f2dff7bcc2573f98a350043af48124e7ed6701b5
Singh <kuldeep.singh@ab-inbev.com>
2023-09-11 19:15:01 +0530

    Logic change in submitted form

--- src/pages/REP_Letters_Module/LetterForm/Functional/FormComponents/ReviewResponsePage.jsx
+++ src/pages/REP_Letters_Module/LetterForm/Functional/FormComponents/ReviewResponsePage.jsx
@@ -6,64 +6,55 @@ import { useDispatch, useSelector } from 'react-redux';
 import CollapseFrame from '../../../../../components/UI/CollapseFrame';
 import Button from '../../../../../components/UI/Button';
 
-const ReviewResponsePage = ({ submittedResponses = {} }) => {
+const ReviewResponsePage = ({ submittedResponses = [] }) => {
   const history = useHistory();
 
   return (
     <CollapseFrame title="Section 1 : Please give your responses" active>
-      {Object.keys(submittedResponses)?.map((questionId, index) => {
-        const response = submittedResponses[questionId];
-
-        return (
-          <div key={questionId}>
-            <div className="renderBlockWrapper mt-5">
-              <div className="question-text-section">
-                <div className="question-number"> {index + 1}</div>
-                <div className="question-text">
-                  <p
-                    className="left-aligned-text"
-                    dangerouslySetInnerHTML={{
-                      __html: response.questionText,
-                    }}
-                  />
-                </div>
+      {submittedResponses?.map((item, index) => (
+        <div key={item.questionID}>
+          <div className="renderBlockWrapper mt-5">
+            <div className="question-text-section">
+              <div className="question-number"> {item.questionNumber}</div>
+              <div className="question-text">
+                <p
+                  className="left-aligned-text"
+                  dangerouslySetInnerHTML={{
+                    __html: item.questionText,
+                  }}
+                />
               </div>
-
-              <Divider color="gray" className="renderBlockWrapper_divider_form" size="xs" />
-              <div className="option-section">
-                <Group position="left" spacing="sm">
-                  {['Yes', 'No', 'NA'].map((value) => (
-                    <label key={value}>
-                      <input
-                        type="radio"
-                        value={value}
-                        checked={response.response === value}
-                        readonly
-                      />
-                      <label className="radio-option-label">{value}</label>
-                    </label>
-                  ))}
-                </Group>
-              </div>
-              {response.comment && (
-                <div>
-                  <Form.Group className="mb-3" controlId={`comment-${questionId}`}>
-                    <Form.Control
-                      as="textarea"
-                      placeholder="Enter your comment..."
-                      required
-                      name="comment"
-                      value={response.comment || ''}
-                      rows={3}
-                      readonly
-                    />
-                  </Form.Group>
-                </div>
-              )}
             </div>
+
+            <Divider color="gray" className="renderBlockWrapper_divider_form" size="xs" />
+            <div className="option-section">
+              <Group position="left" spacing="sm">
+                {['Yes', 'No', 'NA'].map((value) => (
+                  <label key={value}>
+                    <input type="radio" value={value} checked={item.response === value} readOnly />
+                    <label className="radio-option-label">{value}</label>
+                  </label>
+                ))}
+              </Group>
+            </div>
+            {item.comment && (
+              <div>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Enter your comment..."
+                    required
+                    name="comment"
+                    value={item.comment || ''}
+                    rows={3}
+                    readOnly
+                  />
+                </Form.Group>
+              </div>
+            )}
           </div>
-        );
-      })}
+        </div>
+      ))}
       <div>
         <div className="rep-letter-form-bottom-btn">
           <Button
