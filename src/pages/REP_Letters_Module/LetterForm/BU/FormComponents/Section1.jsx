e3f18f29161702b01bb9513798110a640511b5bd
Singh <kuldeep.singh@ab-inbev.com>
2023-10-25 15:57:17 +0530

    fixes and modification accroding to UAT feedback 2

--- src/pages/REP_Letters_Module/LetterForm/BU/FormComponents/Section1.jsx
+++ src/pages/REP_Letters_Module/LetterForm/BU/FormComponents/Section1.jsx
@@ -139,22 +139,22 @@ const Section1 = ({ questions, scopeData }) => {
   }, [DraftResponseState?.data]);
 
   const handleSaveDraft = () => {
-    //console.log('Submitted responses:', responses);
-    if (DraftResponseState?.data?.Attempt_no >= 5) {
-      Swal.fire(`You don't have a limited`, '', 'error');
-      return;
-    }
+    // //console.log('Submitted responses:', responses);
+    // if (DraftResponseState?.data?.Attempt_no >= 5) {
+    //   Swal.fire(`You don't have a limited`, '', 'error');
+    //   return;
+    // }
     Swal.fire({
       title: `Do you want save as draft!`,
-      text: `${
-        DraftResponseState?.data?.Attempt_no
-          ? DraftResponseState?.data?.Attempt_no < 5
-            ? 4 - DraftResponseState?.data?.Attempt_no
-            : 0
-          : DraftResponseState?.data?.Attempt_no === 0
-          ? '4'
-          : '5'
-      } save draft letter remaining`,
+      // text: `${
+      //   DraftResponseState?.data?.Attempt_no
+      //     ? DraftResponseState?.data?.Attempt_no < 5
+      //       ? 4 - DraftResponseState?.data?.Attempt_no
+      //       : 0
+      //     : DraftResponseState?.data?.Attempt_no === 0
+      //     ? '4'
+      //     : '5'
+      // } save draft letter remaining`,
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: 'golden',
@@ -198,23 +198,24 @@ const Section1 = ({ questions, scopeData }) => {
     } else {
       Swal.fire({
         title: 'You are about to submit the Representation letter.',
-        text: `${
-          DraftResponseState?.data?.Attempt_no
-            ? DraftResponseState?.data?.Attempt_no < 5
-              ? 4 - DraftResponseState?.data?.Attempt_no
-              : 0
-            : DraftResponseState?.data?.Attempt_no === 0
-            ? '4'
-            : '5'
-        } save draft letter remaining`,
+        // text: `${
+        //   DraftResponseState?.data?.Attempt_no
+        //     ? DraftResponseState?.data?.Attempt_no < 5
+        //       ? 4 - DraftResponseState?.data?.Attempt_no
+        //       : 0
+        //     : DraftResponseState?.data?.Attempt_no === 0
+        //     ? '4'
+        //     : '5'
+        // } save draft letter remaining`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: 'golden',
         cancelButtonColor: 'black',
         confirmButtonText: 'Yes, submit it!',
-        showDenyButton: !(DraftResponseState?.data?.Attempt_no >= 5),
+        showDenyButton: true,
+        // showDenyButton: !(DraftResponseState?.data?.Attempt_no >= 5),
         denyButtonText: 'Save draft!',
-        denyButtonColor: 'silver',
+        denyButtonColor: 'black',
       }).then((result) => {
         if (result.isConfirmed) {
           for (const key in responses) {
@@ -247,10 +248,10 @@ const Section1 = ({ questions, scopeData }) => {
           //localStorage.setItem('storedResponses', JSON.stringify(responses));
         }
         if (result.isDenied) {
-          if (DraftResponseState?.data?.Attempt_no >= 5) {
-            Swal.fire(`You don't have a limit`, '', 'error');
-            return;
-          }
+          // if (DraftResponseState?.data?.Attempt_no >= 5) {
+          //   Swal.fire(`You don't have a limit`, '', 'error');
+          //   return;
+          // }
           const payload = {
             Assessment_ID: scopeData?.id,
             Latest_response: responses,
