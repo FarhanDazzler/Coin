e04a52c4ae70edddf78ad582182b26e481b97964
Singh <kuldeep.singh@ab-inbev.com>
2023-10-26 14:06:39 +0530

    Functional rep letter double api call fix in mdm

--- src/pages/REP_Letters_Module/Home/MDM/FunctionalMasterdataManagement/FunctionalMasterdataManagementLandingPage.jsx
+++ src/pages/REP_Letters_Module/Home/MDM/FunctionalMasterdataManagement/FunctionalMasterdataManagementLandingPage.jsx
@@ -2,18 +2,9 @@ import React, { useEffect } from 'react';
 import PageWrapper from '../../../../../components/wrappers/PageWrapper';
 import '../../../../MDM/MDMStyle.scss';
 import NavTabsMDM from '../MDM_Tab_Buttons/TabButtons';
-import { useDispatch, useSelector } from 'react-redux';
 import FunctionalMasterdataTable from './Tables/FunctionalMasterdataTables';
-import { getRlFunctionalMasterdata } from '../../../../../redux/REP_Letters/RLMDM/RLMDMAction';
-import EditIcon from '@mui/icons-material/Edit';
 
 const RL_MDM_FunctionalMasterdataManagementLandingPage = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getRlFunctionalMasterdata());
-  }, [])
-
   return (
     <PageWrapper>
       <div className="col-12 col-lg-12">
