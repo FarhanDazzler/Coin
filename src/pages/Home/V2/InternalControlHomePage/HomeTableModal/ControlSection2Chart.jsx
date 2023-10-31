de69d6b6c60352b17a0649bc031d0e3a6960727b
Jaymin <jaymin.patel-ext@ab-inbev.com>
2023-11-01 02:13:35 +0530

    fix: design issues

--- src/pages/Home/V2/InternalControlHomePage/HomeTableModal/ControlSection2Chart.jsx
+++ src/pages/Home/V2/InternalControlHomePage/HomeTableModal/ControlSection2Chart.jsx
@@ -1,38 +1,151 @@
-import React from 'react';
-import Chart from 'react-apexcharts';
+import React, { useEffect, useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
 
 const ControlSection2Chart = () => {
+  const [series, setSeries] = useState([]);
+  const data = {
+    kips: ['KIP_xyz', 'KIP_abc', 'KIP_def'],
+    receivers: ['india', 'china', 'nepal'],
+    data: {
+      KIP_xyz: {
+        MICS_L1_Threshold: '-',
+        MICS_L2_Threshold: '0.05',
+        MICS_L3_Threshold: '0.01',
+        receivers: {
+          india: {
+            jan: '0.7',
+            feb: '0.9',
+            march: '0.5',
+          },
+          nepal: {
+            jan: '0.2',
+            feb: '0.3',
+            march: '0.5',
+          },
+          china: {
+            jan: '0.4',
+            feb: '0.8',
+            march: '0.7',
+          },
+        },
+      },
+      KIP_abc: {
+        MICS_L1_Threshold: '-',
+        MICS_L2_Threshold: '0.05',
+        MICS_L3_Threshold: '0.01',
+        receivers: {
+          india: {
+            jan: '0.7',
+            feb: '0.9',
+            march: '0.5',
+          },
+          nepal: {
+            jan: '0.2',
+            feb: '0.3',
+            march: '0.5',
+          },
+          china: {
+            jan: '0.4',
+            feb: '0.8',
+            march: '0.7',
+          },
+        },
+      },
+      KIP_def: {
+        MICS_L1_Threshold: '-',
+        MICS_L2_Threshold: '0.05',
+        MICS_L3_Threshold: '0.01',
+        receivers: {
+          india: {
+            jan: '0.7',
+            feb: '0.9',
+            march: '0.5',
+          },
+          nepal: {
+            jan: '0.2',
+            feb: '0.3',
+            march: '0.5',
+          },
+          china: {
+            jan: '0.4',
+            feb: '0.8',
+            march: '0.7',
+          },
+        },
+      },
+    },
+  };
+
+  useEffect(() => {
+    const activeKey = data.kips[0];
+    const oneUser = data.data[activeKey];
+    const series = {};
+    Object.keys(oneUser.receivers).map((d, i) => {
+      console.log('@@@@@@', oneUser.receivers[d], d);
+    });
+    return {
+      name: 'Net Profit',
+      data: [44, 55, 57, 56],
+    };
+  }, [data]);
+
   return (
     <div className="controlSection2ChartWrapper w-full">
-      <Chart
+      <ReactApexChart
+        type="area"
         options={{
           chart: {
-            id: 'apexchart-example',
+            type: 'line',
+            height: 350,
           },
-          type: 'line',
-          xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+              endingShape: 'rounded',
+            },
           },
           dataLabels: {
             enabled: false,
           },
           stroke: {
-            curve: 'straight',
+            show: true,
+            width: 2,
+            colors: ['transparent'],
           },
-          grid: {
-            row: {
-              colors: ['#505050', 'transparent'], // takes an array which will be repeated on columns
-              opacity: 0.5,
+          xaxis: {
+            categories: ['Jan', 'Feb', 'March'],
+          },
+          yaxis: {
+            title: {
+              text: '$ (thousands)',
+            },
+          },
+          fill: {
+            opacity: 1,
+          },
+          tooltip: {
+            y: {
+              formatter: function (val) {
+                return '$ ' + val + ' thousands';
+              },
             },
           },
         }}
         series={[
           {
-            name: 'series-1',
-            data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+            name: 'india',
+            data: [0.7, 0.2, 0.4],
+          },
+          {
+            name: 'nepal',
+            data: [0.9, 0.3, 0.8],
+          },
+          {
+            name: 'china',
+            data: [0.5, 0.5, 0.7],
           },
         ]}
-        type="area"
         height={350}
       />
     </div>
