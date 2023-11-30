49c35a88343931e206542b60b6b89455611cd464
Singh <kuldeep.singh@ab-inbev.com>
2023-11-30 16:47:13 +0530

    Admin pannel ZIC added

--- src/redux/AdminPage/AdminPageAction.js
+++ src/redux/AdminPage/AdminPageAction.js
@@ -3,14 +3,14 @@ import {
   ADD_ADMIN_ROLE_REQUEST,
   MODIFY_ADMIN_ROLE_REQUEST,
   DELETE_ADMIN_ROLE_REQUEST,
-  GET_BU_ALL_ROLES_REQUEST,
-  ADD_BU_ADMIN_ROLE_REQUEST,
-  MODIFY_BU_ADMIN_ROLE_REQUEST,
-  DELETE_BU_ADMIN_ROLE_REQUEST,
-  GET_FUNCTION_ALL_ROLES_REQUEST,
-  ADD_FUNCTION_ADMIN_ROLE_REQUEST,
-  MODIFY_FUNCTION_ADMIN_ROLE_REQUEST,
-  DELETE_FUNCTION_ADMIN_ROLE_REQUEST,
+  GET_BU_ZONE_ROLES_REQUEST,
+  ADD_BU_ZONE_ADMIN_ROLE_REQUEST,
+  MODIFY_BU_ZONE_ADMIN_ROLE_REQUEST,
+  DELETE_BU_ZONE_ADMIN_ROLE_REQUEST,
+  GET_FUNCTION_ZONE_ROLES_REQUEST,
+  ADD_FUNCTION_ZONE_ADMIN_ROLE_REQUEST,
+  MODIFY_FUNCTION_ZONE_ADMIN_ROLE_REQUEST,
+  DELETE_FUNCTION_ZONE_ADMIN_ROLE_REQUEST,
 } from './AdminPageReducer';
 
 export const getAll_Roles = (payload) => ({ type: GET__ALL_ROLES_REQUEST, payload });
@@ -30,39 +30,39 @@ export const deleteAdminRole = (payload) => ({
   payload,
 });
 
-export const get_BU_All_Roles = (payload) => ({ type: GET_BU_ALL_ROLES_REQUEST, payload });
+export const get_BU_Zone_Roles = (payload) => ({ type: GET_BU_ZONE_ROLES_REQUEST, payload });
 
-export const add_BU_AdminRole = (payload) => ({
-  type: ADD_BU_ADMIN_ROLE_REQUEST,
+export const add_BU_Zone_AdminRole = (payload) => ({
+  type: ADD_BU_ZONE_ADMIN_ROLE_REQUEST,
   payload,
 });
 
-export const modify_BU_AdminRole = (payload) => ({
-  type: MODIFY_BU_ADMIN_ROLE_REQUEST,
+export const modify_BU_Zone_AdminRole = (payload) => ({
+  type: MODIFY_BU_ZONE_ADMIN_ROLE_REQUEST,
   payload,
 });
 
-export const delete_BU_AdminRole = (payload) => ({
-  type: DELETE_BU_ADMIN_ROLE_REQUEST,
+export const delete_BU_Zone_AdminRole = (payload) => ({
+  type: DELETE_BU_ZONE_ADMIN_ROLE_REQUEST,
   payload,
 });
 
-export const get_Function_All_Roles = (payload) => ({
-  type: GET_FUNCTION_ALL_ROLES_REQUEST,
+export const get_Function_Zone_Roles = (payload) => ({
+  type: GET_FUNCTION_ZONE_ROLES_REQUEST,
   payload,
 });
 
-export const add_Function_AdminRole = (payload) => ({
-  type: ADD_FUNCTION_ADMIN_ROLE_REQUEST,
+export const add_Function_Zone_AdminRole = (payload) => ({
+  type: ADD_FUNCTION_ZONE_ADMIN_ROLE_REQUEST,
   payload,
 });
 
-export const modify_Function_AdminRole = (payload) => ({
-  type: MODIFY_FUNCTION_ADMIN_ROLE_REQUEST,
+export const modify_Function_Zone_AdminRole = (payload) => ({
+  type: MODIFY_FUNCTION_ZONE_ADMIN_ROLE_REQUEST,
   payload,
 });
 
-export const delete_Function_AdminRole = (payload) => ({
-  type: DELETE_FUNCTION_ADMIN_ROLE_REQUEST,
+export const delete_Function_Zone_AdminRole = (payload) => ({
+  type: DELETE_FUNCTION_ZONE_ADMIN_ROLE_REQUEST,
   payload,
 });
