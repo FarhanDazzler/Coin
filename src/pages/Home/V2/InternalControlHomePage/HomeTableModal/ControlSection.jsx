e761ad6d76bc3c17866086814958686d81aee5cf
Jaymin <jaymin.patel-ext@ab-inbev.com>
2023-12-05 23:24:00 +0530

    fix: area and column chart

--- src/pages/Home/V2/InternalControlHomePage/HomeTableModal/ControlSection.jsx
+++ src/pages/Home/V2/InternalControlHomePage/HomeTableModal/ControlSection.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import CollapseFrame from '../../../../../components/UI/CollapseFrame';
 import { useTranslation } from 'react-i18next';
 import CustomTextarea from '../../../../../components/UI/CustomTextarea';
